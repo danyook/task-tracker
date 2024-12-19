23340f768dc734f24483f53f7895ea4537e43af8
Daniil <tdm290705@gmail.com>
2024-12-19 23:47:10 +0300

    refactor

--- src/main/java/ru/kpfu/itis/util/PassEncrypt.java
+++ src/main/java/ru/kpfu/itis/util/PassEncrypt.java
@@ -9,18 +9,17 @@ public class PassEncrypt {
     public PassEncrypt() {}
 
     public static String encrypt(String password) {
-//        MessageDigest md;
-//
-//        try {
-//            md = MessageDigest.getInstance("MD5");
-//            md.update(password.getBytes());
-//            byte[] digest = md.digest();
-//
-//            return DatatypeConverter.printHexBinary(digest);
-//        } catch (NoSuchAlgorithmException e) {
-//            throw new RuntimeException(e);
-//        }
-        return password;
+        MessageDigest md;
+
+        try {
+            md = MessageDigest.getInstance("MD5");
+            md.update(password.getBytes());
+            byte[] digest = md.digest();
+
+            return DatatypeConverter.printHexBinary(digest);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
