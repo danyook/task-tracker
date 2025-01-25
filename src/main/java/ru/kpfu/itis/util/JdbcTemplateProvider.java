e09dfcb3990abc207d73207e8f6f33ad7c4fc172
Daniil <tdm290705@gmail.com>
2025-01-25 12:39:00 +0300

    refactoring

--- src/main/java/ru/kpfu/itis/util/JdbcTemplateProvider.java
+++ src/main/java/ru/kpfu/itis/util/JdbcTemplateProvider.java
@@ -3,24 +3,31 @@ package ru.kpfu.itis.util;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class JdbcTemplateProvider {
+
+
 
     private static JdbcTemplate jdbcTemplate;
 
     public static JdbcTemplate getJdbcTemplate() {
+
+        String PROD_DB_HOST = System.getenv("PROD_DB_HOST");
+        String PROD_DB_PORT = System.getenv("PROD_DB_PORT");
+        String PROD_DB_NAME = System.getenv("PROD_DB_NAME");
+        String PROD_DB_USERNAME = System.getenv("PROD_DB_USERNAME");
+        String PROD_DB_PASSWORD = System.getenv("PROD_DB_PASSWORD");
 
         if (jdbcTemplate == null) {
             DbConfig dbConfig = new DbConfig();
 
             DriverManagerDataSource dataSource = new DriverManagerDataSource();
             dataSource.setDriverClassName(DbConfig.getDatabaseDriver());
-            dataSource.setUrl(DbConfig.getDatabaseUrl());
-            dataSource.setUsername(DbConfig.getDatabaseUsername());
-            dataSource.setPassword(DbConfig.getDatabasePassword());
+
+            dataSource.setUrl("jdbc:postgresql://%s:%s/%s"
+                    .formatted(PROD_DB_HOST, PROD_DB_PORT, PROD_DB_NAME));
+
+            dataSource.setUsername(PROD_DB_USERNAME);
+            dataSource.setPassword(PROD_DB_PASSWORD);
             jdbcTemplate = new JdbcTemplate(dataSource);
         }
 
