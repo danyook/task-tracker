4b1c61276a8a9c29bab04e854aea5ba9814ebcaf
Daniil <tdm290705@gmail.com>
2025-02-07 21:45:15 +0300

    ref

--- src/main/java/ru/kpfu/itis/InitListener.java
+++ src/main/java/ru/kpfu/itis/InitListener.java
@@ -24,9 +24,7 @@ public class InitListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         try {
 
-            ConnectionProvider connectionProvider = ConnectionProvider.getInstance();
 
-            JdbcTemplate jdbcTemplate = JdbcTemplateProvider.getJdbcTemplate();
 
             SectionDAO sectionDAO = SectionDAO.getInstance();
             TaskDAO taskDAO = TaskDAO.getInstance();
@@ -49,6 +47,10 @@ public class InitListener implements ServletContextListener {
             sce.getServletContext().setAttribute("teamService", teamService);
             sce.getServletContext().setAttribute("userService", userService);
             sce.getServletContext().setAttribute("cloudinaryService", cloudinaryService);
+
+            ConnectionProvider connectionProvider = ConnectionProvider.getInstance();
+
+            JdbcTemplate jdbcTemplate = JdbcTemplateProvider.getJdbcTemplate();
 
         } catch (DbException e) {
             log.info("Initialization failed");
